66c566d75c371c018bc4e0d3bf5d34f2a9f91e39
leandroputini <leandroputini@gmail.com>
2018-09-26 14:12:36 -0300

    Condicionais adicionadas, botões ligar/desligar do alarme retirados

--- main.js
+++ main.js
@@ -24,43 +24,47 @@ const dbRef = db.doc('sensores/sensoresStatus');
 console.log(sensor1status);*/
 
 
+
+
 //***** SENSOR 1 *****//
 dbRef.onSnapshot(function(doc){
   var res = (doc.data().sensor1status);
-  console.log(res);
   if (res == true) {
     document.getElementById('sensor1status').innerText = "Status: Ligado";
-    console.log("Status1: Ligado");
+    
   }else{
     document.getElementById('sensor1status').innerText = "Status: Desligado";
-    console.log("Status1: Desligado");
   }
 });
 dbRef.onSnapshot(function(doc){
   var res = (doc.data().sensor1alert);
-  console.log(res);
   if (res == true) {
     document.getElementById('sensor1alert').innerHTML = '<img src="sources/sensor1Alert.png" alt="sensor1Alert" class="planta" >';
     document.getElementById('sensor1status').innerText = "Status: ALERTA!";
-    console.log("Alert1: Ligado");
   }else{
     document.getElementById('sensor1alert').innerHTML = '';
-    console.log("Alert1: Desligado");
   }
 });
+var ress1t = dbRef.get({
+
+})
+console.log(ress1t); 
+
 function ligarSensor1(){
   dbRef.update({
     sensor1status: true
   })
 }
 function desligarSensor1(){
-  dbRef.update({
-    sensor1status: false
-  })
-}
-function ligarAlert1(){
-  dbRef.update({
-    sensor1alert: true
+  dbRef.get().then(function(doc){
+    var r = doc.data().sensor1alert
+    if (r==false) {
+      dbRef.update({
+        sensor1status: false
+      })  
+    }else{
+      console.log("sensor1 não pode ser desligado - em alerta");
+    }
   })
 }
 function desligarAlert1(){
@@ -68,31 +72,41 @@ function desligarAlert1(){
     sensor1alert: false
   })
 }
+function ligarAlert1(){
+  dbRef.get().then(function(doc){
+    var r = doc.data().sensor1status;
+    if (r==true) {
+      dbRef.update({
+        sensor1alert: true
+      })
+      pane();
+      console.log("Alerta do sensor1 ativado");  
+    }else{
+      console.log("Não foi possível alertar sensor1 - desligado.");
+    }
+  });
+  
+}
+
 
 
 //***** SENSOR 2 *****//
 dbRef.onSnapshot(function(doc){
   var res = (doc.data().sensor2status);
-  console.log(res);
   if (res == true) {
     document.getElementById('sensor2status').innerText = "Status: Ligado";
-    console.log("Status2: Ligado");
   }else{
     document.getElementById('sensor2status').innerText = "Status: Desligado";
-    console.log("Status2: Desligado");
   }
 });
 dbRef.onSnapshot(function(doc){
   var res = (doc.data().sensor2alert);
-  console.log(res);
   if (res == true) {
     document.getElementById('sensor2alert').innerHTML = '<img src="sources/sensor2Alert.png" alt="sensor2Alert" class="planta" >';
     document.getElementById('sensor2status').innerText = "Status: ALERTA!";
-    console.log("Alert2: Ligado");
   }else{
     document.getElementById('sensor2alert').innerHTML = '';
-    console.log("Alert2: Desligado");
-  }
+    }
 });
 function ligarSensor2(){
   dbRef.update({
@@ -100,14 +114,30 @@ function ligarSensor2(){
   })
 }
 function desligarSensor2(){
-  dbRef.update({
-    sensor2status: false
+  dbRef.get().then(function(doc){
+    var r = doc.data().sensor2alert
+    if (r==false) {
+      dbRef.update({
+        sensor2status: false
+      })  
+    }else{
+      console.log("sensor2 não pode ser desligado - em alerta");
+    }
   })
 }
 function ligarAlert2(){
-  dbRef.update({
-    sensor2alert: true
-  })
+  dbRef.get().then(function(doc){
+    var r = doc.data().sensor2status;
+    if (r==true) {
+      dbRef.update({
+        sensor2alert: true
+      })
+      console.log("Alerta do sensor2 ativado"); 
+      pane(); 
+    }else{
+      console.log("Não foi possível alertar sensor2 - desligado.");
+    }
+  });
 }
 function desligarAlert2(){
   dbRef.update({
@@ -119,25 +149,19 @@ function desligarAlert2(){
 //***** SENSOR 3 *****//
 dbRef.onSnapshot(function(doc){
   var res = (doc.data().sensor3status);
-  console.log(res);
   if (res == true) {
     document.getElementById('sensor3status').innerText = "Status: Ligado";
-    console.log("Status3: Ligado");
   }else{
     document.getElementById('sensor3status').innerText = "Status: Desligado";
-    console.log("Status3: Desligado");
   }
 });
 dbRef.onSnapshot(function(doc){
   var res = (doc.data().sensor3alert);
-  console.log(res);
   if (res == true) {
     document.getElementById('sensor3alert').innerHTML = '<img src="sources/sensor3Alert.png" alt="sensor3Alert" class="planta" >';
     document.getElementById('sensor3status').innerText = "Status: ALERTA!";
-    console.log("Alert3: Ligado");
   }else{
     document.getElementById('sensor3alert').innerHTML = '';
-    console.log("Alert3: Desligado");
   }
 });
 function ligarSensor3(){
@@ -146,14 +170,30 @@ function ligarSensor3(){
   })
 }
 function desligarSensor3(){
-  dbRef.update({
-    sensor3status: false
+  dbRef.get().then(function(doc){
+    var r = doc.data().sensor3alert
+    if (r==false) {
+      dbRef.update({
+        sensor3status: false
+      })  
+    }else{
+      console.log("sensor3 não pode ser desligado - em alerta");
+    }
   })
 }
 function ligarAlert3(){
-  dbRef.update({
-    sensor3alert: true
-  })
+  dbRef.get().then(function(doc){
+    var r = doc.data().sensor3status;
+    if (r==true) {
+      dbRef.update({
+        sensor3alert: true
+      })
+      pane();
+      console.log("Alerta do sensor3 ativado");  
+    }else{
+      console.log("Não foi possível alertar sensor3 - desligado.");
+    }
+  });
 }
 function desligarAlert3(){
   dbRef.update({
@@ -162,23 +202,17 @@ function desligarAlert3(){
 }
 
 //***** ALARME *****//
+
 dbRef.onSnapshot(function(doc){
   var res = (doc.data().alarmeStatus);
-  console.log(res);
   if (res == true) {
     document.getElementById('alarmeStatus').innerText = "Status: Ligado";
-    console.log("StatusA: Ligado");
-  }else{
-    document.getElementById('alarmeStatus').innerText = "Status: Desligado";
-    console.log("StatusA: Desligado");
   }
 });
 dbRef.onSnapshot(function(doc){
   var res = (doc.data().alarmeAlert);
-  console.log(res);
   if (res == true) {
     document.getElementById('alarmeStatus').innerText = "Status: ALERTA!";
-    console.log("StatusA: Ligado");
   }
 });
 function ligarAlarme(){
